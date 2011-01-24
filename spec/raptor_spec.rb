90853ce853602b9837a9045bde643197eb3b1f0e
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-01-24 18:05:02 +0100

    Clone Raptor & Object into the Unstable namespace

--- spec/raptor_spec.rb
+++ spec/raptor_spec.rb
@@ -5,19 +5,19 @@ require File.expand_path('../spec_helper', __FILE__)
   # describe #initialize
 
     # it stores the object
-      should = Raptor::Should.new(true)
+      should = Unstable::Raptor::Should.new(true)
       should.instance_variable_get(:@object).should == true
 
   # describe #==
 
     # it compares the object with the comparison
-      should = Raptor::Should.new(false)
+      should = Unstable::Raptor::Should.new(false)
       should.stubs(:puts)
       should.==(true).should == false
 
     # it prints the result
       mocha_setup
-      should = Raptor::Should.new(true)
+      should = Unstable::Raptor::Should.new(true)
       should.expects(:puts).with(false)
       should.==(false)
       mocha_verify
@@ -28,7 +28,7 @@ require File.expand_path('../spec_helper', __FILE__)
   # describe #should
 
     # it returns an instance of Raptor::Should
-      object = Object.new
+      object = Unstable::Object.new
       should = object.should
       should.class.should == Raptor::Should
       should.instance_variable_get(:@object).should == object
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,21 @@
 require File.expand_path('../../lib/raptor', __FILE__)
-
 require 'mocha'
 
 class Object
   include Mocha::API
+
+  def clone_constants_from(victim)
+    victim.constants.each do |constant|
+      send(:remove_const, constant)
+      const_set(constant, victim.const_get(constant).clone)
+      victim.const_get(constant).clone_constants_from(const_get(constant))
+    end
+  end
 end
+
+module Unstable
+  Raptor = ::Raptor.clone
+  Object = ::Object.clone
+end
+
+Unstable::Raptor.clone_constants_from Raptor
